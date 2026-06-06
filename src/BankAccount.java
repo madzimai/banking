2d8228b884dd71dba509f80379db7cf8ba06691b
Netsai <hellonetsai@gmail.com>
2026-06-06 12:04:25 +0200

    Bank account class with modified fields - stores data and behavior - constructors intialize objects wn created - method define what an objects can do

---
+++ src/BankAccount.java
@@ -0,0 +1,42 @@
+public class BankAccount {
+    private String owner;
+    private double balance;
+    private String accountNumber;
+
+    public BankAccount(String owner, double balance , String accountNumber) {
+        this.owner = owner;
+        this.balance = balance;
+        this.accountNumber = accountNumber;
+    }
+
+    public void deposit(double amount) {
+        if (amount <= 0) {
+            System.out.println("Invalid amount");
+            return;
+        }
+
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > balance) {
+            System.out.println("Insufficient funds");
+            return;
+        }
+
+        balance -= amount;
+    }
+
+    public String getAccountNumber() {
+        if(accountNumber == null) {
+            System.out.println("Account number not set");
+        }
+        return accountNumber;
+    }
+
+    public void printDetails() {
+        System.out.println("Owner: " + owner);
+        System.out.println("Balance: $" + balance);
+        System.out.println("Account number: " + accountNumber);
+    }
+}
