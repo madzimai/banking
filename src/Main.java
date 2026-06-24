14032c06b0ac487ac2c3064e7192f993939f7fbe
Netsai <hellonetsai@gmail.com>
2026-06-24 15:25:34 +0200

    Search Account feature

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,55 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
 
-            List<BankAccount> accounts = new ArrayList<>();
+        List<BankAccount> accounts = new ArrayList<>();
 
-            accounts.add(
-                    new BankAccount("Netsai", 500, "ACC001"));
+        accounts.add(
+                new BankAccount(
+                        "Netsai",
+                        500,
+                        "ACC001"));
 
-            accounts.add(
-                    new BankAccount("Tariro", 100, "ACC0341"));
+        accounts.add(
+                new BankAccount(
+                        "Tariro",
+                        100,
+                        "ACC0341"));
 
-            accounts.add(
-                    new BankAccount("John", 150, "ACC001JJ"));
+        accounts.add(
+                new BankAccount(
+                        "John",
+                        150,
+                        "ACC001JJ"));
 
-            System.out.println("Total Accounts: "
-                    + accounts.size());
+        System.out.println("Total Accounts: "
+                + accounts.size());
+
+        BankAccount found =
+                findAccount(accounts, "ACC0341");
+
+        if (found != null) {
+            found.printDetails();
+        } else {
+            System.out.println("Account not found");
         }
     }
+
+    public static BankAccount findAccount(
+            List<BankAccount> accounts,
+            String accountNumber) {
+
+        for (BankAccount account : accounts) {
+
+            if (account.getAccountNumber()
+                    .equals(accountNumber)) {
+
+                return account;
+            }
+        }
+
+        return null;
+    }
+}
